76d7e38af5eff0bbb2936a0ce252badcf503798f
Arthur Klis <daniel.klis325@o2.pl>
2024-01-25 21:20:11 +0100

    Added prompt for name in source file.

--- source.py
+++ source.py
@@ -1,3 +1,4 @@
+name=input("Enter your name: ")
 weight=int(input("Enter your weight in pounds: "))
 height=int(input("Enter your height in inches: "))
 bmi_calculated_value=(weight*703)/(height*height)
