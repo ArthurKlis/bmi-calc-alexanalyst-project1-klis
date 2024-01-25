4e5ff4581bd5ed6a129a5ba449bbd79b4ae23463
Arthur Klis <daniel.klis325@o2.pl>
2024-01-25 21:23:57 +0100

    Changed etiquettes for ranges of BMI.

--- source.py
+++ source.py
@@ -4,16 +4,16 @@ height=int(input("Enter your height in inches: "))
 bmi_calculated_value=(weight*703)/(height*height)
 if bmi_calculated_value>0:
     if(bmi_calculated_value<18.5):
-        print("You are underweight")
+        print(name+", you are underweight")
     elif (bmi_calculated_value<=24.9):
-        print("You are normal weight")
+        print(name+", you are normal weight")
     elif (bmi_calculated_value<=29.9):
-        print("You are overweight.")
+        print(name+", you are overweight.")
     elif (bmi_calculated_value<=34.9):
-        print("You are obese")
+        print(name+", you are obese")
     elif (bmi_calculated_value<=39.9):
-        print("You are severely obese")
+        print(name+", you are severely obese")
     elif (bmi_calculated_value>=40):
-        print("You are morbidly obese")
+        print(name+", you are morbidly obese")
     else:
         print("Enter valid inputs")
